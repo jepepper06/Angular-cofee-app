3f6c295d394771f520d5ac8b6e452af3d00181fd
juansebas <juansebastiancastilloojeda@gmail.com>
2022-10-07 13:40:21 -0400

    It was in the wrong file hehe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CardComponent } from './card/card.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,6 +9,10 @@ export class CardComponent implements OnInit {
 
   constructor() { }
 
+  text: string = `We are a brand committed with our client,
+   with give them the best experience in coffe. We know how
+    important coffee could for our incredible and working clients`
+
   ngOnInit(): void {
   }
 

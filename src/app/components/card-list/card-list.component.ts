12f62f885781b30e4468924e4f5f0bcdd2622cab
juansebas <juansebastiancastilloojeda@gmail.com>
2022-10-08 14:26:17 -0400

    Product card-list is working so fine

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -4,16 +4,21 @@ import { ICard } from 'src/app/models/Card.model';
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.scss']
+  styleUrls: ['./card-list.component.scss'],
 })
 export class CardListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  cardsContent: ICard[] = [
-
+  cards: ICard[] = [
+    {
+      img: '../../assets/images/cafe1.jpg',
+      paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      img: '../../assets/images/cafe2.jpg',
+      paragraph:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    },
   ];
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
